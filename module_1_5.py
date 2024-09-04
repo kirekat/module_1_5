de2230fc90172b1b18d951eb61ada91f53e7c331
kirekat <kec85@mail.ru>
2024-09-04 05:16:36 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1.4, int, 8, True
+print(immutable_var)
+#immutable_var_[0][0]=4
+print(type(immutable_var))
+mutable_list = [2, 5, False, 6.9]
+print(mutable_list)
+mutable_list [1] = 8
+print(mutable_list)
